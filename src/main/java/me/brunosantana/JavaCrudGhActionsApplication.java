59ea6806be112129c133dcbe6e4a869997741a69
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-11-01 17:41:04 -0300

    Empty project (initial commit)

---
+++ src/main/java/me/brunosantana/JavaCrudGhActionsApplication.java
@@ -0,0 +1,13 @@
+package me.brunosantana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCrudGhActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCrudGhActionsApplication.class, args);
+	}
+
+}
