8d0e50b63b56bd694c6ecb097b0dd0af58528836
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-11-07 02:48:09 -0300

    Add integration test using Testcontainers WireMock Module

---
+++ src/test/java/me/brunosantana/integration/AlbumControllerTest.java
@@ -0,0 +1,54 @@
+package me.brunosantana.integration;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.wiremock.integrations.testcontainers.WireMockContainer;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Testcontainers
+class AlbumControllerTest {
+
+  @LocalServerPort private Integer port;
+
+  @Container
+  static WireMockContainer wiremockServer =
+      new WireMockContainer("wiremock/wiremock:2.35.0")
+          .withMapping("photos-by-album", AlbumControllerTest.class, "mocks-config.json");
+
+  @DynamicPropertySource
+  static void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("photos.api.base-url", wiremockServer::getBaseUrl);
+  }
+
+  @BeforeEach
+  void setUp() {
+    RestAssured.port = port;
+  }
+
+  @Test
+  void shouldGetAlbumById() {
+    Long albumId = 1L;
+
+    given()
+        .contentType(ContentType.JSON)
+        .when()
+        .get("/albums/{albumId}", albumId)
+        .then()
+        .statusCode(200)
+        .body("albumId", is(albumId.intValue()))
+        .body("photos", hasSize(1));
+  }
+}
